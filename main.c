8a33d8d0d3eb9ba777d26c9bbc4626564a9625b9
ssimm333 <stanislavsimanovich1@gmail.com>
2024-12-05 12:52:05 +0000

    calum please fix

--- main.c
+++ main.c
@@ -192,174 +192,90 @@ int main() {
 	////return 0;
 
 	printMenu();
-	
-	// want this application to run until the user terminates it, hence a do while loop
+
+	int choice, index, input, index1, index2, index3;
+
 	do {
-
-
-		// user input
-		int choice;
-
-		// index choice
-		int index;
-		int input;
-
-		int index1;
-		int index2;
-		int index3;
-
-		// choosing a function
 		printf("\nYour choice: ");
-		scanf_s("%i", &choice);
+		scanf_s("%d", &choice);
 
-		
-
-		// switch statement
 		switch (choice) {
-
 		case 1:
-			// 1) create an empty Ordered Set
-
-			// prompts user to enter the index for the new array in SetsArray
 			printf("\nPlease enter index (0 - 9) for set to be created: ");
-			// read index
 			scanf_s("%d", &index);
-
-			// create an empty Ordered Set at index in SetsArray specified by user
-			// if index is NULL, create an empty Ordered Set
 			if (setsArray[index] == NULL) {
 				setsArray[index] = createOrderedSet();
 				printf("\nSet at index %d created\n", index);
 			}
 			else {
-				// if index is not NULL, print error
 				printf("\nEmpty Ordered Set already exists at index %d in SetsArray.\n", index);
 			}
 			break;
 
 		case 2:
-
-			// 2) delete the current Ordered 
-
-			// prompts user to enter the index for the new array to be deleted
 			printf("\nEnter index (0 - 9) for set to be deleted: ");
-			// read index
 			scanf_s("%d", &index);
-
 			deleteOrderedSet(setsArray[index]);
+			setsArray[index] = NULL;
+			printf("\nSet at index %d deleted\n", index);
 			break;
 
 		case 3:
-
-			// 3) add Elements to Ordered Set
-			//		prompts user to enter the index in SetsArray of the set to be used
-			//		(reads from stdin).
 			printf("\nEnter index (0 - 9) for set to be used: ");
-
-			// read index
 			scanf_s("%d", &index);
-
-
-			//		Then, reads numbers continunously form stdin and uses the function 
-			//		addElement() to attempt to add the number to the Ordered Set.
-			//		This process will stop if a negative number is entered	
-			scanf_s("%d", &input);
-			while (input >= 0) {
+			printf("\nEnter elements to add (negative number to stop): ");
+			while (scanf_s("%d", &input) && input >= 0) {
 				addElement(setsArray[index], input);
-				scanf_s("%d", &input);
-
-				//		For each numberm it will report whether addELement() returned NUMBER_ADDED
-				//		or NUMBER_ALREADY_IN_SET. 
-				addElement(setsArray[index], input);
+				if (addElement == NumberAdded) {
+					printf("NUMBER ADDED\n");
+				}
+				else {
+					printf("NUMBER ALREADY IN SET\n");
+				}
 			}
-
-			//		Finally, it will call printToStdout() to print the resulting set
-			//		(after the last element has been added) to stdout to the screen.
 			printToStdout(setsArray[index]);
-
+			break;
 
 		case 4:
-
-			// 4) Remove elements from Ordered Set
-			//		this function first prompts the user to enter the index in SetsArray of the set to be used
-			//		(that is, it reads it from stdin).
 			printf("\nEnter index (0 - 9) for set to be used: ");
 			scanf_s("%d", &index);
-
-			//		Then it reads numbers continously from stdin and uses function removeElement() to attempt 
-			//		to remove the number from the Ordered Set.
-			do {
-				removeElement(setsArray[index], index);
-				scanf_s("%d", &input);
-				enum removeElementResult result = removeElement(setsArray[index], input);
-			} while (input >= 0);
-
-			//		This process will stop if a negative number is entered. For each number it will report whether 
-			//		removeElemenmt() returned NUMBER REMOVED or NUMBER NOT IN SET.At the end, it will call 
-			//		printToStdout() to print the resulting set(after the last element has been removed) to stdout.
+			printf("\nEnter elements to remove (negative number to stop): ");
+			while (scanf_s("%d", &input) && input >= 0) {
+				removeElement(setsArray[index], input);
+				if (removeElement == NumberRemoved) {
+					printf("NUMBER REMOVED\n");
+				}
+				else {
+					printf("NUMBER NOT IN SET\n");
+				}
+			}
 			printToStdout(setsArray[index]);
 			break;
 
 		case 5:
-
-			// 5) Set Intersection.
-			//		This option prompts the user to enter three indices i1, i2 and i3 for three sets in SetsArray.
-			printf("\nEnter three indeces i1, i2 and i3 to be used: ");
+			printf("\nEnter three indices i1, i2 and i3 to be used: ");
 			scanf_s("%d %d %d", &index1, &index2, &index3);
-
-			//		Then it calls function setIntersection(s1, s2) to obtain the intersection of sets i1 and i2 
 			setsArray[index3] = setIntersection(setsArray[index1], setsArray[index2]);
-			//		and stores the resulting set at index i3.Finally, it prints the resulting set to stdout.
 			printToStdout(setsArray[index3]);
-
 			break;
 
 		case 6:
-
-			// 6) Set Union
-			//		This option prompts the user to enter three indices i1, i2 and i3 for three sets in SetsArray.
-			printf("\nEnter three indeces i1, i2 and i3 to be used: ");
+			printf("\nEnter three indices i1, i2 and i3 to be used: ");
 			scanf_s("%d %d %d", &index1, &index2, &index3);
-
-			//		Then it calls function setUnion(s1, s2) to obtain the union of sets i1 and i2 and stores 
-			//		the resulting set at index i3.
-			setsArray[index3] = setUnion(index1, index2);
-
-			//		Finally, it prints the resulting set to stdout
-			printf("Union of sets %d and %d: ", index1, index2);
+			setsArray[index3] = setUnion(setsArray[index1], setsArray[index2]);
 			printToStdout(setsArray[index3]);
-			printf("\n");
-
 			break;
 
 		case 7:
-
-
-			// 7) Set Difference
-
-			//		This option prompts the user to enter three indices i1, i2 and i3 for three sets in SetsArray.
-			printf("\nEnter three indeces i1, i2 and i3 to be used: ");
+			printf("\nEnter three indices i1, i2 and i3 to be used: ");
 			scanf_s("%d %d %d", &index1, &index2, &index3);
-
-			//		Then it calls function setDifference(s1,s2) to obtain the difference of sets i1 and i2
-			//		(make sure to obtain i1 − i2 and not i2 − ii) and stores the resulting set at index i3.
 			setsArray[index3] = setDifference(setsArray[index1], setsArray[index2]);
-
-			//		Finally, it prints the resulting set to stdout.
 			printToStdout(setsArray[index3]);
 			break;
 
-			printf("\nEnter three indeces i1, i2 and i3 to be used: ");
-			scanf_s("%d %d %d", &index1, &index2, &index3);
-
-			break;
-
 		case 8:
-
-			// 8) Terminate the program
 			printf("\nTerminating program\n");
 			return EXIT_SUCCESS;
-
 
 		default:
 			printf("\nInvalid input\n");
@@ -367,15 +283,10 @@ int main() {
 		}
 
 		printf("\nPress Enter to continue\n");
+		while (getchar() != '\n');
+		getchar();
 
-		// clear buffer
-		scanf_s("*c");
+	} while (1);
 
-		// user input
-		scanf_s("*c");
-
-	} while (EXIT_FAILURE);
-
-	
-
+	return EXIT_FAILURE;
 }
